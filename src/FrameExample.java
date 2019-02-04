5b935b1591f50724602bef69454d036304022684
Jovi <42977182+jovanneli@users.noreply.github.com>
2019-02-04 13:33:32 -0500

    Add files via upload

--- src/FrameExample.java
+++ src/FrameExample.java
@@ -1,7 +1,11 @@
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -10,11 +14,22 @@ public class FrameExample
 	public static void main(String[] args) 
 	{
 		JFrame myFrame = new JFrame();
-		myFrame.setBounds(250, 200, 500, 300);
+		myFrame.setBounds(400, 400, 600, 300);
 		myFrame.setLayout(null);
-		JLabel label1 = new JLabel("Our first label");
-		label1.setLocation(new Point(100, 200));
+		JLabel label1 = new JLabel("Click the button");
+		label1.setBounds(40, 30, 100, 30);
 		myFrame.add(label1);
+		JButton button1 = new JButton("click");
+		button1.setLocation(new Point(180, 30));
+		button1.setSize(new Dimension(100, 30));
+		myFrame.add(button1);
+		button1.addActionListener(new ActionListener() //creating an anonymous class
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						label1.setText("Congrats");
+					}
+				});
 		
 		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
